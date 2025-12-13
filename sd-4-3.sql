7b2447a153ff0e8d8e748eaeff67abac51d7eb09
t41g4 <c4p31048@bunkyo.ac.jp>
2025-12-13 11:01:42 +0900

    DB更新

---
+++ sd-4-3.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2025 年 12 月 13 日 03:01
+-- サーバのバージョン： 10.4.28-MariaDB
+-- PHP のバージョン: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `sd-4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `markers`
+--
+
+CREATE TABLE `markers` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `taken_at` datetime DEFAULT NULL,
+  `photo_path` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `markers`
+--
+
+INSERT INTO `markers` (`id`, `user_id`, `name`, `latitude`, `longitude`, `taken_at`, `photo_path`, `created_at`) VALUES
+(2, 5, '自販機1', 35.396320217291745, 139.46608453989032, NULL, '', '2025-12-01 17:45:10'),
+(3, 5, '自販機２', 35.39602941986086, 139.46510285139084, NULL, '', '2025-12-01 18:14:42'),
+(4, 5, '自販機3', 35.39689087687094, 139.46483194828036, NULL, '', '2025-12-01 18:54:27'),
+(5, 5, '自販機4', 35.3977785615221, 139.46810960769656, NULL, '', '2025-12-01 19:39:04'),
+(6, 5, '変更してみたよ', 35.39865311812369, 139.464408159256, '2025-12-13 04:18:56', '/static/uploads/LINE_ALBUM__251015_1.jpg', '2025-12-01 21:16:14'),
+(8, 6, 'ローソン前', 35.397231958828975, 139.46268081665042, '2025-12-02 12:02:00', '', '2025-12-02 03:02:48'),
+(12, 5, '渋谷原公園前', 35.39995618986267, 139.46381807327273, '2025-12-10 03:29:00', '/static/uploads/S__132800912.jpg', '2025-12-12 18:29:55');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `image_url`) VALUES
+(1, 'コーラ', '/images/coke.png'),
+(2, '水', '/images/water.png'),
+(3, 'ポカリ', '/images/pocari.png'),
+(4, '伊右衛門', '/images/iyemon.png'),
+(5, '伊右衛門 濃い茶', '/images/iyemon_koicha.png'),
+(6, '伊右衛門 ほうじ茶', '/images/iyemon_hojicha.png'),
+(7, '伊右衛門 特茶', '/images/iyemon_tokucha.png'),
+(8, '烏龍茶', '/images/oolong_tea.png'),
+(9, 'サントリー天然水', '/images/suntory_tennensui.png'),
+(10, 'サントリー天然水 朝摘みオレンジ', '/images/tennensui_orange.png'),
+(11, 'GREEN DA・KA・RA', '/images/green_dakara.png'),
+(12, 'GREEN DA・KA・RA やさしい麦茶', '/images/dakara_mugicha.png'),
+(13, 'デカビタC', '/images/decavita_c.png'),
+(14, 'C.C.レモン', '/images/cc_lemon.png'),
+(15, 'C.C.レモン 塩', '/images/cc_lemon_salt.png'),
+(16, 'ペプシコーラ', '/images/pepsi.png'),
+(17, 'ペプシ ジャパンコーラ', '/images/pepsi_japan.png'),
+(18, 'なっちゃん オレンジ', '/images/natchan_orange.png'),
+(19, 'なっちゃん りんご', '/images/natchan_apple.png'),
+(20, 'なっちゃん ぶどう', '/images/natchan_grape.png'),
+(21, 'BOSS ブラック', '/images/boss_black.png'),
+(22, 'BOSS 無糖', '/images/boss_muto.png'),
+(23, 'BOSS カフェオレ', '/images/boss_cafeau_lait.png'),
+(24, 'BOSS レインボーマウンテン', '/images/boss_rainbow.png'),
+(25, 'BOSS 贅沢微糖', '/images/boss_zeitaku.png'),
+(26, 'BOSS プレミアムボス', '/images/premium_boss.png'),
+(27, 'おしるこ', '/images/oshiruko.png'),
+(28, 'コーンポタージュ', '/images/corn_potage.png'),
+(29, 'レモンスカッシュ', '/images/lemon_squash.png'),
+(30, 'ジンジャーエール', '/images/ginger_ale.png'),
+(31, 'なっちゃん みかんスパークリング', '/images/natchan_sparkling.png');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `restocks`
+--
+
+CREATE TABLE `restocks` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `marker_id` int(11) NOT NULL,
+  `quantity` double NOT NULL,
+  `restocked_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `restocks`
+--
+
+INSERT INTO `restocks` (`id`, `user_id`, `product_id`, `marker_id`, `quantity`, `restocked_at`) VALUES
+(1, 5, 2, 3, 12, '2025-12-12 08:14:28');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `routes`
+--
+
+CREATE TABLE `routes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `start_lat` double NOT NULL,
+  `start_lng` double NOT NULL,
+  `goal_lat` double NOT NULL,
+  `goal_lng` double NOT NULL,
+  `mode` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `routes`
+--
+
+INSERT INTO `routes` (`id`, `name`, `start_lat`, `start_lng`, `goal_lat`, `goal_lng`, `mode`, `created_at`) VALUES
+(1, 'テスト1', 35.4017664626273, 139.4635605812073, 35.39559212851686, 139.46315288543704, 'optimized', '2025-12-10 14:19:20'),
+(2, '登録１', 35.401399164194, 139.4633460044861, 35.396134369430555, 139.45926904678348, 'manual', '2025-12-13 01:18:34'),
+(3, '登録２', 35.39205872746594, 139.46405410766604, 35.39653667484895, 139.4674873352051, 'manual', '2025-12-13 01:20:08'),
+(4, '登録３', 35.3690097, 139.4186557, 35.3967028438925, 139.46771264076236, 'optimized', '2025-12-13 01:21:38'),
+(5, '登録４', 35.3690097, 139.4186557, 35.3967028438925, 139.46771264076236, 'optimized', '2025-12-13 01:22:39');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `route_markers`
+--
+
+CREATE TABLE `route_markers` (
+  `id` int(11) NOT NULL,
+  `route_id` int(11) NOT NULL,
+  `marker_id` int(11) DEFAULT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `order_num` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `route_markers`
+--
+
+INSERT INTO `route_markers` (`id`, `route_id`, `marker_id`, `lat`, `lng`, `order_num`) VALUES
+(1, 1, 6, 35.39865311812369, 139.464408159256, 1),
+(2, 1, 8, 35.397231958828975, 139.46268081665042, 2),
+(3, 2, 12, 35.39995618986267, 139.46381807327273, 1),
+(4, 2, 6, 35.39865311812369, 139.464408159256, 2),
+(5, 2, 8, 35.397231958828975, 139.46268081665042, 3),
+(6, 3, 3, 35.39602941986086, 139.46510285139084, 1),
+(7, 3, 2, 35.396320217291745, 139.46608453989032, 2),
+(8, 3, 4, 35.39689087687094, 139.46483194828036, 3),
+(9, 4, 2, 35.396320217291745, 139.46608453989032, 1),
+(10, 4, 3, 35.39602941986086, 139.46510285139084, 2),
+(11, 4, 8, 35.397231958828975, 139.46268081665042, 3),
+(12, 4, 12, 35.39995618986267, 139.46381807327273, 4),
+(13, 4, 5, 35.3977785615221, 139.46810960769656, 5),
+(14, 5, 2, 35.396320217291745, 139.46608453989032, 1),
+(15, 5, 3, 35.39602941986086, 139.46510285139084, 2),
+(16, 5, 8, 35.397231958828975, 139.46268081665042, 3),
+(17, 5, 12, 35.39995618986267, 139.46381807327273, 4),
+(18, 5, 4, 35.39689087687094, 139.46483194828036, 5);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login_id` varchar(100) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `users`
+--
+
+INSERT INTO `users` (`id`, `login_id`, `password_hash`, `created_at`) VALUES
+(0, 'kanrisya', 'scrypt:32768:8:1$4PhMpqwPGFaulZHL$a85f52219fc3e328d55024e5d7637c9fb107a42abb7266f963a69234029a8682201fcbeb4e18634e7464d51785a2b6a246bf54942df7b607a4a46207a0e25bc2', '2025-12-09 05:32:28'),
+(5, 'c4p31048', 'scrypt:32768:8:1$lDaNhSQYhO1mWFaX$68fa20435a1b761801e22d8054a8b941f4d2efa44780de2f14bfbdb73251aa1277d63aecf7c86c4180e1bbe66efb7a89f73c87d64a84a388ff3c6c7c096b2e3f', '2025-11-29 13:37:02'),
+(6, 'demo', 'scrypt:32768:8:1$TMSc6wCeWIqMRGv2$235e91a52817a05c717905d1385c0efa588d26441127ecf4c99ccbfec9a25ba3aba4d894254ae8d35f2acd84983f5175c81d36dfa4b9f7ae7b6e2e08ef0aba90', '2025-12-02 03:02:10'),
+(7, 'neko', 'scrypt:32768:8:1$kyZYxd4lfr9BSNmK$b9748195730b83f9e452eab3e17f77d6caaf2fa421256b0fb86c0b0130a71002bb30d31538b11dd829ebac7de9071edd463a795bb1b7361e25a31c386c94f120', '2025-12-02 03:05:33'),
+(8, 'c4p31114', 'scrypt:32768:8:1$4PhMpqwPGFaulZHL$a85f52219fc3e328d55024e5d7637c9fb107a42abb7266f963a69234029a8682201fcbeb4e18634e7464d51785a2b6a246bf54942df7b607a4a46207a0e25bc2', '2025-12-09 05:32:28');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `markers`
+--
+ALTER TABLE `markers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- テーブルのインデックス `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `restocks`
+--
+ALTER TABLE `restocks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `marker_id` (`marker_id`);
+
+--
+-- テーブルのインデックス `routes`
+--
+ALTER TABLE `routes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `route_markers`
+--
+ALTER TABLE `route_markers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `route_id` (`route_id`);
+
+--
+-- テーブルのインデックス `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login_id` (`login_id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `markers`
+--
+ALTER TABLE `markers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- テーブルの AUTO_INCREMENT `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- テーブルの AUTO_INCREMENT `restocks`
+--
+ALTER TABLE `restocks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- テーブルの AUTO_INCREMENT `routes`
+--
+ALTER TABLE `routes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- テーブルの AUTO_INCREMENT `route_markers`
+--
+ALTER TABLE `route_markers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- テーブルの AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `markers`
+--
+ALTER TABLE `markers`
+  ADD CONSTRAINT `markers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- テーブルの制約 `restocks`
+--
+ALTER TABLE `restocks`
+  ADD CONSTRAINT `restocks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `restocks_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `restocks_ibfk_3` FOREIGN KEY (`marker_id`) REFERENCES `markers` (`id`);
+
+--
+-- テーブルの制約 `route_markers`
+--
+ALTER TABLE `route_markers`
+  ADD CONSTRAINT `rm_route_fk` FOREIGN KEY (`route_id`) REFERENCES `routes` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
